17fdba1b902fb82db339a255f4b90ceee306877f
Kazuya Takei <attakei@gmail.com>
2016-09-13 11:31:15 +0900

    Add sample server

---
+++ server.py
@@ -0,0 +1,10 @@
+# -*- coding:utf8 -*-
+from bottle import route, run, template
+
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+
+run(host='localhost', port=8080)
